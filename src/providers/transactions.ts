1816f7525d62a65a4ed21047698ffaf1031e030b
Dmytro Khalin <d.khalin@gmail.com>
2018-02-07 12:15:18 +0200

    loading timeout quickfix

--- src/providers/transactions.ts
+++ src/providers/transactions.ts
@@ -34,5 +34,5 @@ export class TransactionsProvider extends BaseProvider {
   cancelTransaction(id) {
     return this.makeRawRequest('post', 'transactions/' + id + '/' + 'user_cancel');
   }
-  
+
 }
