983253047c1157c5c4131f30ec94771ba8f609d6
Martin Nordlinder <nordlinder@gmail.com>
2019-01-19 14:11:41 +0100

    ny server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
-var path = require('path');
+var cors = require('cors');
 var app = express();
-app.use(express.static(path.resolve(__dirname, "www")));
+app.use(cors());
+app.use(express.static("www"));
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
   console.log("listening to Port", app.get("port"));
